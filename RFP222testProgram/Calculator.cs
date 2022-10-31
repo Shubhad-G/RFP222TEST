99b28c3b5b14bff9676327075ddb98c68a549082
Shubhad Gonal <shubhadgonal19@gmail.com>
2022-10-31 22:21:49 +0530

    displayed the welcome message

---
+++ RFP222testProgram/Calculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RFP222testProgram
+{
+    internal class Calculator
+    {
+        public static int Addition(int a,int b)
+        {
+            return a + b;  
+        }
+    }
+}
---
+++ RFP222testProgram/Humans.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RFP222testProgram
+{
+    internal class Humans
+    { 
+       
+      
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome");
+            Console.ReadLine();
+            
+
+
+        }
+
+    }
+
+}
